c22215893833f48618e81b7917587fb774327e4b
Hernameis <msh4287@naver.com>
2022-05-21 17:45:08 +0900

    category added

--- src/js/amgiking_dbHandle.js
+++ src/js/amgiking_dbHandle.js
@@ -7,6 +7,7 @@ const defaultVal = null;
 
 let index = 0;
 let indexArr = [];
+let category_id= 1;
 let questionList = null;
 
 function initVariables() {
@@ -24,7 +25,7 @@ function openDB() {
 // execute creating question table transaction
 function createQuestionTable() {
     db.transaction(function(ps) {
-        const createTableSQL = 'CREATE TABLE IF NOT EXISTS question(question_id integer primary key autoincrement, question text, answer text, category text, right integer default 0, wrong integer default 0)';
+        const createTableSQL = 'CREATE TABLE IF NOT EXISTS question(question_id integer primary key autoincrement, question text, answer text, category_id integer default 1, right integer default 0, wrong integer default 0)';
         ps.executeSql(createTableSQL, [],
             function() {
                 console.log('2_1_Succeed in creating question table sql');
@@ -57,6 +58,25 @@ function createCategoryTable() {
     );
 }
 
+// execute insert into categoy table default row
+function insertDefaultRowInCategoryTable() {
+        db.transaction(function(ps) {
+            const insertSQL = 'insert into category(category_name) select "기본" where not exists (select * from category where category_name="기본")';
+            ps.executeSql(insertSQL, [],
+            function() {
+                console.log('Succeed in inserting default row in category table sql');
+            },function() {
+                console.log('Failed inserting default row in category table sql');
+            }
+        );
+    },function() {
+        console.log('Failed inserting default row in category table transaction');
+    },function(){
+        console.log('Succeed in inserting default row in category table transaction');
+    }
+    );
+}
+
 // execute insert data transaction
 function insertQuestion() {
     db.transaction(function(ps) {
@@ -66,9 +86,9 @@ function insertQuestion() {
         if (isEmptyStr(question)) { alert('문제를 입력해주세요'); return -1; }
         if (isEmptyStr(answer)) { alert('정답을 입력해주세요'); return -2; }
 
-        let insertSQL = 'insert into question(question, answer, category, right, wrong) values(?,?,?,?,?)';
+        let insertSQL = 'insert into question(question, answer, category_id, right, wrong) values(?,?,?,?,?)';
 
-        ps.executeSql(insertSQL, [question, answer, defaultVal, defaultNum, defaultNum],
+        ps.executeSql(insertSQL, [question, answer, category_id, defaultNum, defaultNum],
             function(ps, rs) {
                 console.log('3_책 등록 no : ' + rs.insertId);
                 alert('질문 "' + question +'"' + ' 이 등록되었습니다');
@@ -82,8 +102,8 @@ function insertQuestion() {
 // execute get qestion tranaction
 function getQuestion() {
     db.transaction(function(ps) {
-        const selectCntQuestion = 'select * from question';
-        ps.executeSql(selectCntQuestion, [], function(ps, rs) {
+        const selectCntQuestion = 'select * from question where category_id=?';
+        ps.executeSql(selectCntQuestion, [category_id], function(ps, rs) {
             let cnt = rs.rows.length;
             for(i=0; i<cnt; i++) {
                 indexArr[i] = i;
@@ -104,11 +124,11 @@ function getQuestion() {
 }
 
 // execute select question list transaction
-function listQuestion() {
+function getQuestionList() {
     db.transaction(function(ps) {
         console.log('select question list');
-        const selctQuestionSQL = 'select * from question';
-        ps.executeSql(selctQuestionSQL, [], function(ps, rs) {
+        const selctQuestionSQL = 'select * from question where category_id=?';
+        ps.executeSql(selctQuestionSQL, [category_id], function(ps, rs) {
             const len = rs.rows.length;
             if (len == 0) {
                 document.getElementById('list-ul').innerHTML='<li>저장된 질문이 없습니다</li>';
@@ -127,8 +147,28 @@ function listQuestion() {
         console.log('5_question list transaction failed');
     },function() {
         console.log('5_question list transaction succeed')
-    }
-    );
+    });
+}
+
+function categoryList() {
+    db.transaction(function(ps) {
+        console.log('select category list');
+        const selctQuestionSQL = 'select * from category';
+        ps.executeSql(selctQuestionSQL, [], function(ps, rs) {
+            const len = rs.rows.length;
+            document.getElementById('list-ul').innerHTML='';
+            $('#select-category').empty();
+            for (i=0; i<len; i++) {
+                let option = $("<option value=\""+rs.rows.item(i).category_id+"\" >"+rs.rows.item(i).category_name+"</option>");
+                $('#select-category').append(option);
+                
+            }
+        });
+    },function() {
+        console.log('category list transaction failed');
+    },function() {
+        console.log('category list transaction succeed')
+    });
 }
 
 function responseAnswer() {
@@ -184,7 +224,7 @@ function clearInputQuestion() {
 
 function deleteAllDatabases() {
     db.transaction(function(ps) {
-        deleteSQL = 'delete from question';
+        deleteSQL = 'drop table question';
         ps.executeSql(deleteSQL, [],function() {
                 initVariables();
                 console.log('deleting all databases sql succeed');
